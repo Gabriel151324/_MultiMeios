698bae138327b9ac19b118804fae3a88b5eef8b2
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2026-02-26 15:48:42 -0300

    Update

--- script.js
+++ script.js
@@ -40,3 +40,27 @@ const blurHeader = () => {
     : header.classList.remove("blur-header");
 };
 window.addEventListener("scroll", blurHeader);
+
+//*=============== Entrar ===============*/
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const user = document.getElementById("login-username").value;
+    const pass = document.getElementById("login-password").value;
+
+    const response = await fetch("https://api-multimeios.onrender.com/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user, pass }),
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+      window.location.href = "/Assets/Dashboard.html";
+    } else {
+      alert("Credenciais inválidas.");
+    }
+  });
